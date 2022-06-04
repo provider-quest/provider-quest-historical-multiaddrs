58f69385ad4791197d1bd3197e3efb9e230bf415
Jim Pick <jim@jimpick.com>
2022-06-03 18:45:19 -0700

    Switch to using observable-prerender forked to work against localhost

--- run-multiaddr-ip-tool.mjs
+++ run-multiaddr-ip-tool.mjs
@@ -1,10 +1,12 @@
-import observableRuntime from "@observablehq/runtime"
-import define from "@jimpick/provider-quest-multiaddr-ip-tool"
-import './fetch-polyfill.mjs'
+import fs from 'fs'
+import url from 'url'
+import { load } from '@jimpick/observable-prerender-localhost'
 import Fastify from 'fastify'
 import fastifyStatic from '@fastify/static'
 import fastifyCors from '@fastify/cors'
 import 'dotenv/config'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const workDir = process.env.WORK_DIR || '.'
 
@@ -16,9 +18,39 @@ fastify.register(fastifyCors, {
   origin: '*'
 })
 
+function renderList (dirs, files) {
+      return `
+<html><body>
+<ul>
+  ${dirs.map(dir => `<li><a href="${dir.href}">${dir.name}</a></li>`).join('\n  ')}
+</ul>
+<ul>
+  ${files.map(file => `<li><a href="${file.href}" target="_blank">${file.name}</a></li>`).join('\n  ')}
+</ul>
+</body></html>
+`
+}
+
 fastify.register(fastifyStatic, {
   root: workDir + '/miner-info-subset-latest/',
-  prefix: '/miner-info-subset-latest/'
+  prefix: '/miner-info-subset-latest',
+  prefixAvoidTrailingSlash: true,
+  list: {
+    format: 'html',
+    render: renderList,
+    names: ['index', 'index.html', '/']
+  }
+})
+
+fastify.register(fastifyStatic, {
+  root: __dirname + '/observable-notebooks/',
+  prefix: '/notebooks/',
+  list: {
+    format: 'html',
+    render: renderList,
+    names: ['index', 'index.html', '/']
+  },
+  decorateReply: false
 })
 
 const startFastify = async () => {
@@ -32,22 +64,25 @@ const startFastify = async () => {
 startFastify()
 
 async function run () {
-  const runtime = new observableRuntime.Runtime()
-  const notebook = runtime.module(define)
+  const notebook = await load(
+    '@jimpick/provider-quest-multiaddr-ip-tool',
+    ['minerMultiaddrIps', 'deltaMultiaddrsIps']
+    // { headless: false }
+  )
 
   const date = '2022-05-12'
   const minerInfoSubsetLatestUrl = `http://localhost:3000/miner-info-subset-latest/miner-info-subset-latest-${date}.json`
 
-  const fetchJs = (await fetch(`${minerInfoSubsetLatestUrl}`)).json()
-  await notebook.redefine('minerInfoSubsetLatest', fetchJs)
+  await notebook.redefine('minerInfoSubsetLatestUrl', minerInfoSubsetLatestUrl)
   await notebook.redefine('dhtAddrsLatest', { miners: {} })
   await notebook.redefine('minTimestamp', new Date('2020-08-23'))
     
   // console.log(await notebook.value("minerInfoSubsetLatest"))
   // console.log(await notebook.value("miners"))
   // console.log(await notebook.value("minersCombined"))
-  console.log(await notebook.value("minerMultiaddrs"))
-  // console.log(await notebook.value("minerMultiaddrIps"))
+  // console.log(await notebook.value("minerMultiaddrs"))
+  console.log(await notebook.value("minerMultiaddrIps"))
+  await notebook.browser.close()
   fastify.close()
 }
 
